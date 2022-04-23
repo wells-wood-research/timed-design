278cabcb1796cf96a486da23467976872caa5109
Leonardo Castorina <leonardo.castorina98@gmail.com>
2022-04-23 13:14:34 +0100

    Move functions to utils folder for easy setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -18,4 +18,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8",
+    packages=find_packages(include=["utils"]),
 )
