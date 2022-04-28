19bb7d6e1dae03c86a45fe4b89ae7296573c8d99
Leonardo Castorina <leonardo.castorina98@gmail.com>
2022-04-28 14:41:03 +0100

    Add sampling util

---
+++ sample.py
@@ -0,0 +1,97 @@
+import argparse
+from pathlib import Path
+
+import numpy as np
+
+from utils.sampling_utils import (
+    random_choice_prob_index,
+    save_as,
+)
+from utils.utils import extract_sequence_from_pred_matrix, get_rotamer_codec
+
+
+def main(args):
+    args.path_to_pred_matrix = Path(args.path_to_pred_matrix)
+    args.path_to_datasetmap = Path(args.path_to_datasetmap)
+    assert (
+        args.path_to_pred_matrix.exists()
+    ), f"Prediction Matrix file {args.path_to_pred_matrix} does not exist"
+    assert (
+        args.path_to_datasetmap.exists()
+    ), f"Dataset Map file {args.path_to_datasetmap} does not exist"
+    prediction_matrix = np.genfromtxt(
+        args.path_to_pred_matrix, delimiter=",", dtype=np.float16
+    )
+    dataset_map = np.genfromtxt(
+        args.path_to_datasetmap,
+        delimiter=",",
+        dtype=str,
+    )
+    if args.predict_rotamers:
+        codec, flat_categories = get_rotamer_codec()
+    else:
+        codec, flat_categories = None, None
+
+    (
+        pdb_to_sequence,
+        pdb_to_probability,
+        pdb_to_real_sequence,
+        _,
+        _,
+    ) = extract_sequence_from_pred_matrix(
+        dataset_map, prediction_matrix, rotamers_categories=flat_categories
+    )
+    # TODO add multiprocessing:
+    pdb_to_sample = {}
+    print(
+        f"Ready to sample {args.sample_n} for each of the {len(list(pdb_to_probability.keys()))} proteins."
+    )
+    for pdb in pdb_to_probability.keys():
+        # Sample from distribution
+        sampled_seq_list = []
+        for i in range(args.sample_n):
+            seq_list = random_choice_prob_index(np.array(pdb_to_probability[pdb]))
+            sampled_seq = "".join(seq_list)
+            sampled_seq_list.append(sampled_seq)
+        pdb_to_sample[pdb] = sampled_seq_list
+    save_as(pdb_to_sample, model_name=args.path_to_pred_matrix.stem, mode=args.save_as)
+
+    raise ValueError
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument(
+        "--path_to_pred_matrix",
+        type=str,
+        help="Path to prediction matrix file ending with .csv",
+    )
+    parser.add_argument(
+        "--path_to_datasetmap",
+        default="datasetmap.txt",
+        type=str,
+        help="Path to dataset map ending with .txt",
+    )
+    parser.add_argument(
+        "--predict_rotamers",
+        type=bool,
+        default=False,
+        help="Whether model outputs predictions for 338 rotamers (True) or 20 residues (False).",
+    )
+    parser.add_argument(
+        "--sample_n",
+        type=int,
+        default=2,
+        help="Number of samples to be drawn from the distribution.",
+    )
+    parser.add_argument(
+        "--save_as",
+        type=str,
+        default="all",
+        const="all",
+        nargs="?",
+        choices=["fasta", "json", "all"],
+        help="Whether to save as fasta and json (default: all) or either of them.",
+    )
+    params = parser.parse_args()
+    main(params)
---
+++ utils/sampling_utils.py
@@ -0,0 +1,53 @@
+import json
+
+import numpy as np
+from ampal.amino_acids import standard_amino_acids
+
+
+def save_as(pdb_to_sampled, model_name, mode):
+    """
+    Saves sampled sequences as fasta, json or both.
+
+    Parameters
+    ----------
+    pdb_to_sampled
+    model_name
+    mode
+
+    Returns
+    -------
+
+    """
+    print(f"Saving sampled sequences in mode {mode}")
+    if mode != "fasta":
+        with open(f"{model_name}.json", "w") as outfile:
+            json.dump(pdb_to_sampled, outfile)
+    if mode != "json":
+        with open(f"{model_name}.fasta", "w") as outfile:
+            for pdb, seq_list in pdb_to_sampled.items():
+                for i, seq in enumerate(seq_list):
+                    outfile.write(f">{pdb}_{i}\n")
+                    outfile.write(f"{seq_list[i]}\n")
+
+
+def random_choice_prob_index(probs, axis=1, return_seq=True):
+    """
+    Code adapted from: https://stackoverflow.com/questions/47722005/vectorizing-numpy-random-choice-for-given-2d-array-of-probabilities-along-an-a?noredirect=1&lq=1
+
+    Parameters
+    ----------
+    probs
+    axis
+    return_seq
+
+    Returns
+    -------
+
+    """
+    r = np.expand_dims(np.random.rand(probs.shape[1 - axis]), axis=axis)
+    idxs = (probs.cumsum(axis=axis) > r).argmax(axis=axis)
+    if return_seq:
+        res = np.array(list(standard_amino_acids.keys()))
+        return res[idxs]
+    else:
+        return idxs
