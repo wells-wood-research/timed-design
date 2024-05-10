ee65e4c8e849957bc6fb07ca86505648c6ca1525
Leonardo Castorina <leonardo.castorina98@gmail.com>
2024-05-10 16:48:16 +0100

    Allow for multiple fasta in a file

--- fix_protease.py
+++ fix_protease.py
@@ -16,17 +16,28 @@ def process_files(input_dir, output_dir, modifications, suffix):
 
     for fasta_file in input_dir.glob('*.fasta'):
         with open(fasta_file, 'r') as file:
-            lines = file.readlines()
-            header = lines[0].strip()
-            sequence = ''.join(line.strip() for line in lines[1:])
+            sequences = []
+            current_sequence = []
+            header = None
+            for line in file:
+                if line.startswith('>'):  # Start of a new sequence
+                    if header and current_sequence:  # Save the previous sequence
+                        sequences.append((header, ''.join(current_sequence)))
+                    # Append suffix to the protein ID in the header
+                    header = line.strip() + suffix
+                    current_sequence = []  # Reset sequence list
+                else:
+                    current_sequence.append(line.strip())
+            if header and current_sequence:  # Save the last sequence if there is one
+                sequences.append((header, ''.join(current_sequence)))
 
-        modified_sequence = modify_fasta_sequence(sequence, modifications)
-        output_content = header + '\n' + modified_sequence
-
-        # Define the new file path with appended suffix
+        # Process each sequence and write to the new file
         new_file_path = output_dir / f'{fasta_file.stem}{suffix}.fasta'
-        with open(new_file_path, 'w') as file:
-            file.write(output_content)
+        with open(new_file_path, 'w') as new_file:
+            for header, sequence in sequences:
+                modified_sequence = modify_fasta_sequence(sequence, modifications)
+                output_content = f'{header}\n{modified_sequence}\n'
+                new_file.write(output_content)
         print(f'Processed {fasta_file.name} to {new_file_path.name}')
 
 def main():
@@ -34,7 +45,7 @@ def main():
     parser.add_argument('--input_dir', type=str, help='Directory containing FASTA files')
     parser.add_argument('--output_dir', type=str, help='Directory to save modified FASTA files')
     parser.add_argument('--modifications', type=str, help='Modifications in the format "A1,B2,C3"')
-    parser.add_argument('--suffix', type=str, default='_modified', help='Suffix to append to file names')
+    parser.add_argument('--suffix', type=str, default='_modified', help='Suffix to append to file names and headers')
 
     args = parser.parse_args()
     process_files(args.input_dir, args.output_dir, args.modifications, args.suffix)
